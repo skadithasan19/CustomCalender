d846d19635123fc60074aec06caaaa0908fa9406
Md Adit Hasan <skadithasan19@gmail.com>
2025-02-25 08:27:23 -0600

    Initial Commit

---
+++ CustomCalender/CustomCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalenderApp.swift
+//  CustomCalender
+//
+//  Created by Adit Hasan on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CustomCalenderTests/CustomCalenderTests.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalenderTests.swift
+//  CustomCalenderTests
+//
+//  Created by Adit Hasan on 2/25/25.
+//
+
+import Testing
+@testable import CustomCalender
+
+struct CustomCalenderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
